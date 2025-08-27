2c7e38dc69b11bdf271be00d6a45d5e18eca13b3
root <root@LAPTOP-2ROJ67VE.localdomain>
2025-08-27 20:48:49 +0530

    initial deployement

---
+++ phase1-demo-main/frontend/src/pages/private-pages/distributor/DistributorDashboard.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import { useVerifyRouteQuery } from "../../../services/LoginRegisterApi";
+import { useNavigate, useParams } from "react-router-dom";
+
+function DistributorDashboard() {
+  const navigate = useNavigate();
+  const { userId } = useParams();
+  const { data, isLoading, error } = useVerifyRouteQuery(userId);
+
+  // Redirect if unauthorized
+  useEffect(() => {
+    if (!isLoading && (error || !data?.status)) {
+      navigate("/unified-health-tech/register");
+    }
+  }, [error, data, isLoading, navigate]);
+
+  if (isLoading) return <div>Loading...</div>;
+  
+  
+  return (
+    <div className="">
+      <h1>Dashboard</h1>
+      <p>Welcome, {data?.user?.userId || userId}</p>
+    </div>
+  );
+}
+
+export default DistributorDashboard;
