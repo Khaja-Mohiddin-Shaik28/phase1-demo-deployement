3fd009e5086e4a56e858248f3385a40f9ed99d16
Khaja Mohiddin Shaik <141769653+Khaja-Mohiddin-Shaik28@users.noreply.github.com>
2025-08-27 21:19:25 +0530

    Update LoginRegisterApi.js

--- phase1-demo-main/frontend/src/services/LoginRegisterApi.js
+++ phase1-demo-main/frontend/src/services/LoginRegisterApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const LoginRegisterApi = createApi({
   reducerPath: "LoginRegisterApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000/api",
+    baseUrl: "https://phase1-demo-deployement.onrender.com/api",
     credentials: "include",
   }),
   endpoints: (builder) => ({
